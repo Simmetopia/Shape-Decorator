51bf8bf08d6a7ec1aae9880be46706cad7b2d04b
Simon Egeberg <201406253@post.au.dk>
2017-12-04 12:26:47 +0100

    inital

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shape_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
